c9c6b42fea0c4c92606b60d835726a630325d766
danaroshchuk <126064287+DanaRoshchuk@users.noreply.github.com>
2025-02-14 13:17:56 +0200

    Update main.tf

--- base/main.tf
+++ base/main.tf
@@ -1,3 +1,38 @@
+
+terraform {
+    required_providers {
+        scalr = {
+            source = "registry.scalr.io/scalr/scalr"
+            version= "2.1.1"
+        }
+    }
+}
+data "scalr_current_account" "data_acc" {}
+
+data "scalr_environment" "data_env" {
+#account_id = data.scalr_current_account.data_acc
+  id = "env-v0on0fdgvjdf8ljta"  # optional, can only use id or name for the environment filter, if both are used there will be a conflict.
+}
+
+
+resource "scalr_variable" "new_varAll" {
+  count = 1
+  key            = "varall" #-${count.index}
+  value          = "TRACE"
+  category       = "terraform"
+  environment_id = data.scalr_environment.data_env.id
+}
+
+resource "scalr_variable" "var_newAll" {
+  key            = "keyall"
+  value          = "1"
+  category       = "shell"
+  environment_id = data.scalr_environment.data_env.id
+}
+
+
+
+
 variable "module_name" {
   description = "The name of the module"
   default = "test_module_name"
